3b14d010a3ac5fe1c0170f343a84e4b72ef886ef
KodingCore <kcorvais@gmail.com>
2023-07-04 08:58:10 +0000

    exo en cours

---
+++ models/User.php
@@ -0,0 +1,77 @@
+<?php
+
+class User{
+    private int $id;
+    private string $firstName;
+    private string $lastName;
+    private string $email;
+    private string $password;
+    
+    public function __construct(string $firstName, string $lastName, string $email, string $password){
+        $this->id = null;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->email = $email;
+        $this->password = $password;
+    }
+    
+    public function getId() : int
+    {
+        return $this->id;
+    }
+    
+    public function getFirstName() : string
+    {
+        return $this->firstName;
+    }
+    
+    public function getLastName() : string
+    {
+        return $this->lastName;
+    }
+    
+    public function getFullName(string $firstName, string $lastName) : string
+    {
+        return $firstName." ".$lastName;
+    } 
+    
+    public function getEmail() : string
+    {
+        return $this->email;
+    }
+    
+    public function getPassword() : string
+    {
+        return $this->password;
+    }
+    
+    public function setId(int $id) : void
+    {
+        $this->id = $id;
+    }
+    
+    public function setFirstName(string $firstName) : void
+    {
+        $this->firstName = $firstName;
+    }
+    
+    public function setLastName(string $lastName) : void
+    {
+        $this->lastName = $lastName;
+    }
+    
+    public function setEmail(string $email) : void
+    {
+        $this->email = $email;
+    }
+    
+    public function setPassword(string $password) : void
+    {
+        $this->password = $password;
+    }
+    
+
+}
+
+
+?>
